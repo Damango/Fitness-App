701052d3fa28a3c56ac8a62105e9df0b5c48a56f
Justin Kessler <redcoderhd1@gmail.com>
2021-04-16 23:13:06 -0400

    Created container for workout page.

--- src/Components/WorkoutsPage/WorkoutsPage.js
+++ src/Components/WorkoutsPage/WorkoutsPage.js
@@ -2,9 +2,11 @@ import React from 'react';
 import "./WorkoutsPage.css"
 const WorkoutsPage = (props) => {
     return ( <div className="workouts-page-container">
+        <div className="workouts-list-container"></div>
+        <div className="statistics-container"></div>
+        <div className="workout-templates-container"></div>
 
 
-        <button>CLICK ME</button>
     </div> );
 }
  

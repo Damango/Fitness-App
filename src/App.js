5e703cb52f7a2b8f236ca77d04a27633f6754472
Justin Kessler <redcoderhd1@gmail.com>
2021-05-04 23:43:19 -0400

    Delete Workout Button moved

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   //'http://194.195.215.144:3000'
 
-  const theConnection = 'http://194.195.215.144:3000'
+  const theConnection = 'http://localhost:5000'
 
 
 
@@ -25,7 +25,7 @@ function App() {
       return(<LandingPage changeView={setViewState} setUserData={setUserData} userData={userData} connection={theConnection}/>)
     }
     else if(viewState === 'workouts-page'){
-      return(<WorkoutsPage data={userData} fuck='fuck' setUserData={setUserData} workouts={userData.workouts} changeView={setViewState} theConnection={'http://localhost:5000'}/>)
+      return(<WorkoutsPage data={userData} setUserData={setUserData} workouts={userData.workouts} changeView={setViewState} theConnection={'http://localhost:5000'}/>)
     }
     else if(viewState === 'login-page'){
       return(<LoginPage changeView={setViewState} setUserData={setUserData} connection={theConnection}/>)
